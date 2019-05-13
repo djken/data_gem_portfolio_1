cf53949c1258576641f72321b72ff30fd76fdee0
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-13 04:35:14 -0400

    first commit

---
+++ lib/data_gem_portfolio.rb
@@ -0,0 +1,6 @@
+require "data_gem_portfolio/version"
+
+module DataGemPortfolio
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/data_gem_portfolio/version.rb
@@ -0,0 +1,3 @@
+module DataGemPortfolio
+  VERSION = "0.1.0"
+end
